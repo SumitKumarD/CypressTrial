6cffe6f93c7f98751202454ee9240be22a9f7567
Sumit kumar <dahia.sumitkumar@gmail.com>
2023-08-14 08:21:01 +0530

    Second Commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("cypress");
-watchForFileChanges=false
-trashAssetsBeforeRuns=true
+const {downloadFile} = require('cypress-downloadfile/lib/addPlugin')
 module.exports = defineConfig({
   projectId: '1xo1pv',
   e2e: {
+    watchForFileChanges: false,
+    trashAssetsBeforeRuns: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('task', {downloadFile})
     },
   },
 });
